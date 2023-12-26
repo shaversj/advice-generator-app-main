6ccc961113955ea38ca21499c91c15210b7520ee
Josh Shavers <shaversj@gmail.com>
2023-12-26 16:30:00 -0600

    Desktop and Mobile changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
+import desktopDivider from "/src/assets/images/pattern-divider-desktop.svg";
+import mobileDivider from "/src/assets/images/pattern-divider-mobile.svg";
+import dice from "/src/assets/images/icon-dice.svg";
 
-import desktopDivider from '/src/assets/images/pattern-divider-desktop.svg'
-import mobileDivider from '/src/assets/images/pattern-divider-mobile.svg'
-import dice from '/src/assets/images/icon-dice.svg'
 function App() {
-
   return (
     <>
-      <div className={"bg-black min-h-screen grid place-items-center"}>
-          <div className={"max-w-screen-sm w-full bg-dark-blue font-Manrope min-h-[667px]"}>
-              <main className={"pt-[118px] px-4"}>
-                  <div className={"bg-dark-grayish-blue rounded-lg px-5"}>
-                      <h2 className={"text-neon-green text-center pt-[40px] uppercase tracking-[.2rem] text-xs"}>Advice #117</h2>
-                      <p className={"text-light-cyan font-semibold text-2xl text-center pt-[32px]"}>
-                          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-                      </p>
-                      <picture className={"flex justify-center pt-[20px] pb-[60px]"}>
-                          <source srcSet={desktopDivider} media={"(min-width: 1440px)"}/>
-                          <img src={mobileDivider} alt={""}/>
-                      </picture>
-                  </div>
-                  <div className={"flex justify-center -mt-8"}>
-                      <div className={"bg-neon-green rounded-full w-16 h-16 flex items-center justify-center"}>
-                          <img src={dice} alt={""}/>
-                      </div>
-                  </div>
-
-              </main>
-          </div>
+      <div className={"grid min-h-screen place-items-center bg-black"}>
+        <div className={"min-h-[667px] w-full max-w-screen-sm bg-dark-blue font-Manrope lg:grid lg:min-h-[810px] lg:max-w-screen-lg lg:place-items-center"}>
+          <main className={"px-4 pt-[118px] lg:px-[450px] lg:pt-0"}>
+            <div className={"rounded-lg bg-dark-grayish-blue px-5 lg:rounded-2xl lg:px-[40px]"}>
+              <h2 className={"fon pt-[40px] text-center text-[11.5px] font-medium uppercase tracking-[.215rem] text-neon-green lg:pt-[49px] lg:text-[12.5px] lg:font-bold lg:tracking-[.27rem]"}>Advice #117</h2>
+              <p className={"pt-[26px] text-center text-2xl font-semibold tracking-[0.006em] text-light-cyan lg:pt-[26px] lg:text-[27.8px] lg:font-semibold lg:leading-[2.35rem] lg:tracking-[0.01em]"}>
+                "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+              </p>
+              <picture className={"flex justify-center pb-[60px] pt-[20px] lg:pb-[72px] lg:pt-[40px]"}>
+                <source srcSet={desktopDivider} media={"(min-width: 1440px)"} />
+                <img src={mobileDivider} alt={""} />
+              </picture>
+            </div>
+            <div className={"-mt-8 flex justify-center"}>
+              <div className={"flex h-16 w-16 items-center justify-center rounded-full bg-neon-green"}>
+                <img src={dice} alt={""} />
+              </div>
+            </div>
+          </main>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
